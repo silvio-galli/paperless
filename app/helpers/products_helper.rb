653a71c45b616d3fb3477560cbbb1f7acffcf7c4
silvio-galli <silvio.galli@gmail.com>
2016-06-16 02:19:13 +0200

    Added product helper to update available product pieces after customer order

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,6 @@
 module ProductsHelper
+  def update_quantity(product)
+    ordered_items = product.order_items.map { |item| item.quantity }
+    product.quantity - ordered_items.sum
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
+  has_many :order_items
   default_scope { order('initiative DESC') }
 end
