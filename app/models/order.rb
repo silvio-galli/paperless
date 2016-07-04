8e753ac3ec4869403bd965a03c576e891d9c90d1
Silvio Galli <silvio.galli@gmail.com>
2016-07-05 01:56:01 +0200

    Added customers and orders list to product#show view

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ActiveRecord::Base
   belongs_to :customer
   has_many :order_items
+  has_many :products, through: :order_items
 
   default_scope { order("created_at DESC") }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :order_items
+  has_many :orders, through: :order_items
+  
   default_scope { order('initiative DESC') }
 
   enum status: [:in_stock, :arriving, :arrived]
