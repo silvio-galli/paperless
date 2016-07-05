3c4b0fdc41484905879ea801758f40a3f48b36d6
Silvio Galli <silvio.galli@gmail.com>
2016-07-05 11:51:50 +0200

    Removed product from selection options in order#show view based on its availability and added sold out label for products

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,6 +1,17 @@
 module ProductsHelper
-  def update_quantity(product)
+  def available_items?(product)
     ordered_items = product.order_items.map { |item| item.quantity }
-    product.quantity - ordered_items.sum
+    available_items = product.quantity - ordered_items.sum
   end
+
+  def check_product_availability(product)
+    ordered_items = product.order_items.map { |item| item.quantity }
+    available_items = product.quantity - ordered_items.sum
+    if available_items <= 0
+      "<span class=\"label label-default\">sold out</span>".html_safe
+    else
+      available_items
+    end
+  end
+
 end
