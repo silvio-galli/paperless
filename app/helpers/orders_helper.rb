8427364bdc1b3ddd9e55fe467673537dc78435f8
Silvio Galli <silvio.galli@gmail.com>
2016-07-15 02:27:10 +0200

    Localization mainly completed

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -8,4 +8,12 @@ module OrdersHelper
     items = order.order_items.map { |item| item.quantity }
     items.sum
   end
+
+  def close_button(order)
+    if order.open?
+      link_to t('.close'), order_path(order, :order => { status: :closed }), method: :patch, class: 'btn btn-success btn-sm', data: { confirm: t('.close_order_confirmation') }
+    else
+      "&nbsp;".html_safe
+    end
+  end
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -3,24 +3,24 @@ module WelcomeHelper
     if product.in_stock?
       # status = product.status.gsub(/_/, " ")
       if product.arriving_date == nil
-        "<span class=\"label label-default pull-right\">#{t('recurring.in_stock')}</span>".html_safe
+        "<span class=\"label label-default\">#{t('recurring.in_stock')}</span>".html_safe
       else
-        "<span class=\"label label-success pull-right\">#{t('recurring.in_stock')}</span>".html_safe
+        "<span class=\"label label-success\">#{t('recurring.in_stock')}</span>".html_safe
       end
     elsif product.arriving?
       if arriving_date >= Time.now
-        "<span class=\"label label-warning pull-right\">#{t('recurring.arriving')}</span>".html_safe
+        "<span class=\"label label-warning\">#{t('recurring.arriving')}</span>".html_safe
       else
-        "<span class=\"label label-danger pull-right\">#{t('recurring.delayed')}</span>".html_safe
+        "<span class=\"label label-danger\">#{t('recurring.delayed')}</span>".html_safe
       end
     end
   end
 
   def order_status_label(order)
     if order.open?
-      "<span class=\"label label-warning\">#{order.status}</span>".html_safe
+      "<span class=\"label label-warning\">#{t('recurring.open')}</span>".html_safe
     else
-      "<span class=\"label label-primary\">#{order.status}</span>".html_safe
+      "<span class=\"label label-primary\">#{t('recurring.closed')}</span>".html_safe
     end
   end
 end
