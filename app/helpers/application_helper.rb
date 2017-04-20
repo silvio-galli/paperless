9e6e183b871ff678f88be3edf7bbca0df3c95955
silvio-galli <silvio.galli@gmail.com>
2017-04-20 11:56:24 +0200

    Fixed panel title pluralization for customers  in products#show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,5 @@
 module ApplicationHelper
 
-  def get_label_for(item)
-    if item.count <= 0
-      "<span class=\"label label-default\">#{item.count}</span>".html_safe
-    elsif item.count > 0
-      "<span class=\"label label-primary\">#{item.count}</span>".html_safe
-    end
-  end
-
   def get_user_for_paper_trail(whodunnit)
     whodunnit != nil ? User.find(whodunnit).name : "system"
   end
