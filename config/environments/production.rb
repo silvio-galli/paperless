110d6d83a421d2d9c9d167097e54921c53f192cb
silvio-galli <silvio.galli@gmail.com>
2017-04-28 13:19:07 +0200

    Added mailer settings to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mailer settings
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        'smtp.gmail.com',
+    port:           '587',
+    authentication: :plain,
+    domain:         ENV['GMAIL_DOMAIN'],
+    user_name:      ENV['GMAIL_USERNAME'],
+    password:       ENV['GMAIL_PASSWORD'],
+    enable_starttls_auto: true
 end
