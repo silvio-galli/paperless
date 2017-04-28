87d182cd5c4b6fafe37f60b8ba4450107597aacc
silvio-galli <silvio.galli@gmail.com>
2017-04-28 14:39:02 +0200

    Fixed mailer settings and Procfile with sidekiq start for Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,9 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Mailer settings
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:        'smtp.gmail.com',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mailer settings
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'warm-coast-71743.herokuapp.com' }
+  config.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:        'smtp.gmail.com',
